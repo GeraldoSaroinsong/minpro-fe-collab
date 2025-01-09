20f3876216916111def08cb929d26b6069c53f43
ruben <101534085+rubensiregar@users.noreply.github.com>
2025-01-09 10:23:20 +0700

    ref register (#16)

--- src/app/sign-up/SignUpSchema.ts
+++ src/app/sign-up/SignUpSchema.ts
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 export const SignUpSchema = Yup.object().shape({
-    fullname: Yup.string().required("Name is required"),
+    name: Yup.string().required("Name is required"),
     email: Yup.string().email("Must be an email").required("Email is required"),
     username: Yup.string().required("Username is required"),
     password: Yup.string()
